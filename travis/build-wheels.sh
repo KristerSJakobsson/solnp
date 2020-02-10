0e1a5f4bfb25ceab1f1731112ebb207350caff60
Krister S Jakobsson <krister.s.jakobsson@gmail.com>
2020-02-10 23:26:36 +0900

    Try to fix building errors

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e -x
 
-export PYHOME=/home
-cd ${PYHOME}
-
-/opt/python/cp37-cp37m/bin/pip install twine cmake
-ln -s /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
+yum install -y cmake
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
