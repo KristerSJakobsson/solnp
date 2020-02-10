f7805d8b3c93e42a1f64b42971113bbc34226040
Krister S Jakobsson <krister.s.jakobsson@gmail.com>
2020-02-10 22:47:48 +0900

    Try to fix building errors

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
-yum install -y atlas-devel
-yum install -y cmake
+export PYHOME=/home
+cd ${PYHOME}
+
+/opt/python/cp37-cp37m/bin/pip install twine cmake
+ln -s /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/requirements-dev.txt
+for PYBIN in /opt/python/cp3*/bin; do
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
     "${PYBIN}/python" /io/setup.py sdist -d /io/wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
-for whl in /io/wheelhouse/*; do
-    echo "$whl"
+# Test
+for PYBIN in /opt/python/cp3*/bin/; do
+    "${PYBIN}/pip" install -r /io/requirements-test.txt
+    "${PYBIN}/pip" install --no-index -f /io/wheelhouse nb-cpp
+    (cd "$PYHOME"; "${PYBIN}/pytest" /io/test/)
 done
-
-# Install package and test
-for PYBIN in /opt/python/*/bin; do
-#    ln -s "${PYBIN}/cmake" /usr/bin/cmake
-    "${PYBIN}/pip" install pysolnp --no-index -f /io/wheelhouse
-    (cd "$PYHOME"; "${PYBIN}/nosetests" /io/python_solnp/test/test.py)
-done
-#
-##  Upload
-#for WHEEL in /io/wheelhouse/pysolnp*; do
-#    /opt/python/cp37-cp37m/bin/twine upload \
-#        --skip-existing \
-#        --repository-url "${TWINE_SERVER}" \
-#        --username "${TWINE_USERNAME}" \
-#        --password "${TWINE_PASSWORD}" \
-#        "${WHEEL}"
-#done
