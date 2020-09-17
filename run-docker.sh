522f79e741d2dcca8fcc940b9fe22560df49a7c4
A-fil <abonimation@gmail.com>
2020-09-18 00:45:14 +0900

    Try to change travis runtime

--- run-docker.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -v $(pwd):/io -e TWINE_USERNAME="$PYPI_USERNAME" -e TWINE_PASSWORD="$PYPI_PASSWORD" -e TWINE_SERVER="$PYPI_SERVER" "$DOCKER_IMAGE" "$PRE_CMD" /io/travis/build-wheels.sh
-ls wheelhouse/
