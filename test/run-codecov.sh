d2152b9a6cdcb88286341b6eda0cdf3a21cf129f
A-fil <abonimation@gmail.com>
2020-09-19 14:20:13 +0900

    Try to get travis to run codecov

--- test/run-codecov.sh
+++ test/run-codecov.sh
@@ -1,3 +1,4 @@
-  make gcov
-  make lcov
-  bash <(curl -s https://codecov.io/bash) -X gcov || echo "Codecov did not collect coverage reports"
+cmake .
+make gcov
+make lcov
+bash <(curl -s https://codecov.io/bash) -X gcov || echo "Codecov did not collect coverage reports"
