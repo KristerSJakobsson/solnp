29930d300d7017281f2aaf34601da256d46a2594
A-fil <abonimation@gmail.com>
2020-09-21 16:24:37 +0900

    Try to make the CMake script work with older version of CMake

--- travis/install-catch.sh
+++ travis/install-catch.sh
@@ -1,3 +1,2 @@
 # Custom shell script for installing catch on travis because the apt install path doesn't work with CMake
 sudo apt-get -y install catch
-ln -s /usr/include/catch/catch.hpp /usr/include/catch.hpp
